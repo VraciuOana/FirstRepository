32b1ff7b2ca79f39baf4c670e77b99e2c72e8249
VraciuOana <oana_vraciu@yahoo.com>
2024-08-25 23:02:25 +0300

    homework din session3

--- com/java/session_5_advanced_flow_control/Main.java
+++ com/java/session_5_advanced_flow_control/Main.java
@@ -6,8 +6,16 @@ public class Main {
 
     public static void main(String[] args){
 
-        whileExampleWithContinue();
-        nestedForExample();
+//        whileExampleWithContinue();
+//        nestedForExample();
+//        nestedForExampleWithoutLabel();
+
+        boolean bol = nestedForExampleWithReturn();
+        if (bol) {
+            System.out.println("Found it!");
+        } else System.out.println("Value not found!");
+
+        System.out.println("DONE!");
     }
 
     private static void whileExampleWithContinue() {
@@ -37,12 +45,46 @@ public class Main {
                     break SEARCH;
 
                 }
-                System.out.println(matrix[i][j] + " ");
+                System.out.println(matrix[i][j]);
                 }
             }
         System.out.println("Done!");
-
         }
+
+    private static void nestedForExampleWithoutLabel() {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int searchVal = 5;
+
+        boolean found = false;
+        for (int i=0; i<matrix.length && !found; i++){
+            for (int j=0; j<matrix[i].length;j++){
+                if(matrix[i][j] == searchVal) {
+                    System.out.println("Found it!");
+                    found = true;
+                    break;
+
+                }
+                System.out.println(matrix[i][j]);
+            }
+        }
+        System.out.println("Done!");
+
     }
+
+    private static boolean nestedForExampleWithReturn(){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int searchVal = 5;
+
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix[i].length;j++){
+                if(matrix[i][j] == searchVal) {
+                    return true;
+                }
+                System.out.println(matrix[i][j]);
+            }
+        }
+        return false;
+    }
+}
 
 
