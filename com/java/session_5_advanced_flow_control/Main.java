563f7b02878b4754ea6f83eb510261d357177c6e
VraciuOana <oana_vraciu@yahoo.com>
2024-08-25 22:33:26 +0300

    homework din session3

--- com/java/session_5_advanced_flow_control/Main.java
+++ com/java/session_5_advanced_flow_control/Main.java
@@ -1,11 +1,15 @@
 package session_5_advanced_flow_control;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args){
 
         whileExampleWithContinue();
+        nestedForExample();
     }
+
     private static void whileExampleWithContinue() {
         int i = 0;
         int sum = 0;
@@ -21,4 +25,24 @@ public class Main {
         System.out.println("Sum = " +sum);
     }
 
-}
+    private static void nestedForExample(){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int searchVal = 5;
+
+        SEARCH:
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix[i].length;j++){
+                if(matrix[i][j] == searchVal) {
+                    System.out.println("Found it!");
+                    break SEARCH;
+
+                }
+                System.out.println(matrix[i][j] + " ");
+                }
+            }
+        System.out.println("Done!");
+
+        }
+    }
+
+
