497671d8b41bb09c94bd848bee3cbe4dc88ec199
VraciuOana <oana_vraciu@yahoo.com>
2024-08-26 19:00:34 +0300

    homework din session3

---
+++ com/java/session_6core_api/Main.java
@@ -0,0 +1,10 @@
+package session_6core_api;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        String str2 = "Hello World!";
+
+        System.out.println("str == str2 " + (str == str2));
+    }
+}
