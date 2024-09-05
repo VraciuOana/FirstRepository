a8e0b695e35617c439086b93a1dbb1e3aadb7682
VraciuOana <oana_vraciu@yahoo.com>
2024-09-05 17:48:40 +0300

    homework din session3

--- com/java/session_6core_api/Main.java
+++ com/java/session_6core_api/Main.java
@@ -1,10 +1,31 @@
 package session_6core_api;
 
+import java.util.StringJoiner;
+
 public class Main {
     public static void main(String[] args) {
-        String str = "Hello World!";
-        String str2 = "Hello World!";
 
-        System.out.println("str == str2 " + (str == str2));
+        StringBuilder sb = new StringBuilder("Hello world!");
+        System.out.println("Revers: " + sb.reverse().toString());
+
+        String[] words = {"Sam", "Ion", "Dan", "Bob"};
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            stringBuilder.append(word).append(" ");
+
+        }
+        System.out.println("Concatenated string: " +stringBuilder.toString());
+
+        StringJoiner stringJoiner = new StringJoiner(" ", "[", "]");
+        for (String word : words){
+            stringJoiner.add(word);
+        }
+        System.out.println("Concatenated string: " +stringJoiner.toString());
+
+
+//        String str = "Hello World!";
+//        String str1 = "Hello World!";
+//
+//        System.out.println("str == str2 " + (str == str1));
     }
 }
